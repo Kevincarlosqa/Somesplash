6b4c24625adb31d3d6b48de0936dcc7ef75ca760
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-21 18:46:47 -0500

    add: Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@ puts "Start Seeding"
 
 Photo.destroy_all
 Category.destroy_all
+Comment.destroy_all
 
-ActiveRecord: :Base.connection.reset_pk_sequence!('photos')
-ActiveRecord: :Base.connection.reset_pk_sequence!('categories')
+ActiveRecord::Base.connection.reset_pk_sequence!('photos')
+ActiveRecord::Base.connection.reset_pk_sequence!('categories')
+ActiveRecord::Base.connection.reset_pk_sequence!('comments')
 
 puts "Sedding Categories"
 
@@ -35,4 +37,41 @@ animals = Category.create(
 
 puts "Seeding Photos"
 
-purple_lake = Photo.create
+purple_lake = Photo.create(title: "Purple Lake", description: "Nice and big purple lake.", category: nature)
+green_plant = Photo.create(title: "Green Plant", description: "Nice and big purple lake.", category: nature)
+lonely_bird = Photo.create(title: "Lonely Bird", description: "Nice and big purple lake.", category: nature)
+three = Photo.create(title: "Three", description: "Nice and big purple lake.", category: nature)
+squirrel = Photo.create(title: "Squirrel", description: "Nice and big purple lake.", category: nature)
+universe = Photo.create(title: "Universe", description: "Nice and big purple lake.", category: nature)
+
+desert_sunset = Photo.create(title: "Desert Sunset", description: "Nice and big purple lake.", category: wallpaper)
+osean_sunset = Photo.create(title: "Ocean Sunset", description: "Nice and big purple lake.", category: wallpaper)
+city_sunset = Photo.create(title: "City Sunset", description: "Nice and big purple lake.", category: wallpaper)
+
+people1 = Photo.create(title: "People 1", description: "Nice and big purple lake.", category: people)
+people2 = Photo.create(title: "People 2", description: "Nice and big purple lake.", category: people)
+people3 = Photo.create(title: "People 3", description: "Nice and big purple lake.", category: people)
+people4 = Photo.create(title: "People 4", description: "Nice and big purple lake.", category: people)
+people5 = Photo.create(title: "People 5", description: "Nice and big purple lake.", category: people)
+people6 = Photo.create(title: "People 6", description: "Nice and big purple lake.", category: people)
+
+food1 = Photo.create(title: "Food 1", description: "Nice and big purple lake.", category: food)
+food2 = Photo.create(title: "Food 2", description: "Nice and big purple lake.", category: food)
+food3 = Photo.create(title: "Food 3", description: "Nice and big purple lake.", category: food)
+food4 = Photo.create(title: "Food 4", description: "Nice and big purple lake.", category: food)
+
+technology1 = Photo.create(title: "Technology 1", description: "Nice and big purple lake.", category: technology)
+technology2 = Photo.create(title: "Technology 2", description: "Nice and big purple lake.", category: technology)
+technology3 = Photo.create(title: "Technology 3", description: "Nice and big purple lake.", category: technology)
+technology4 = Photo.create(title: "Technology 4", description: "Nice and big purple lake.", category: technology)
+technology5 = Photo.create(title: "Technology 5", description: "Nice and big purple lake.", category: technology)
+
+animals1 = Photo.create(title: "Animals 1", description: "Nice and big purple lake.", category: animals)
+animals2 = Photo.create(title: "Animals 2", description: "Nice and big purple lake.", category: animals)
+animals3 = Photo.create(title: "Animals 3", description: "Nice and big purple lake.", category: animals)
+animals4 = Photo.create(title: "Animals 4", description: "Nice and big purple lake.", category: animals)
+
+category_comment = Comment.create(comment: "This is an example of a comment for this category.", commentable: nature)
+photo_comment = Comment.create(comment: "This is an example of a comment for this photo.", commentable: purple_lake)
+
+puts "End Seeding"
