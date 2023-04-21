c250555f5e23d61c5a60f4b7e9f5a707ed8a741a
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-21 16:30:44 -0500

    add: validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :photos, dependent: :nullify
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, counter_cache: true, optional: true
+  validates :title, presence: true
 end
