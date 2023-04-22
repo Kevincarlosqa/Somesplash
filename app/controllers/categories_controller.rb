6e1927caf6255c9361e613dd63ef31305c710531
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-21 20:56:49 -0500

    add(category): show controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,14 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
+    render "index"
   end
 
   def new
   end
 
   def show
+    @categories = Category.find(params[:id])
   end
 
   def create
