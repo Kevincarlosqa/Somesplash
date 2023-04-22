a9ddc376ea79bca148efa9eb6c6a275e3b29b281
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-21 20:38:12 -0500

    add(categories): routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/show'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
