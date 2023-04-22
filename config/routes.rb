376612cd26b2d77dd6b50e467a6eee69f3279860
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-21 20:36:47 -0500

    chore(categories): Controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/show'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
